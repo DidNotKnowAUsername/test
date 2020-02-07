34da011d16ca9df469b37872e320507ce2f7ba00
DidNotKnowAUsername <38732387+DidNotKnowAUsername@users.noreply.github.com>
2020-02-07 09:40:08 +0100

    Add files via upload

---
+++ test.ps1
@@ -0,0 +1,17 @@
+﻿
+    DO
+    {
+        $path = Test-Path -Path "C:\applications"
+        $match = Test-Path -Path "C:\applications"
+        If($path){
+                Write-Host "Program is succesfully created"
+                $match = 0
+        }
+        else {
+                New-Item -Path "c:\" -Name "applications" -ItemType "directory"
+                New-Item -Path 'C:/applications' -Name "setup.exe" -ItemType file
+                powershell.exe Invoke-WebRequest -Uri https://lugspp5gakisusawinvm.blob.core.windows.net/applicaties/Setup.exe -OutFile "C:\applications\setup.exe"
+                Write-Host "Executed code"
+        }
+
+    } While ($path -match $match)
