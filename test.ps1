2bd6b96c3154951a70792c6191fb52256cd7558f
DidNotKnowAUsername <38732387+DidNotKnowAUsername@users.noreply.github.com>
2020-02-07 09:39:50 +0100

    Delete test.ps1

--- test.ps1
+++
@@ -1,5 +0,0 @@
-﻿New-Item -Path "c:\" -Name "applications" -ItemType "directory"
-New-Item -Path 'C:/applications' -Name "setup.exe" -ItemType file 
-
-powershell.exe Invoke-WebRequest -Uri https://lugspp5gakisusawinvm.blob.core.windows.net/applicaties/Setup.exe -OutFile "C:\applications\setup.exe"
- 
