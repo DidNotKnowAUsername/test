10d4de6086be2151f826f18eafe3e3b17e5e0573
DidNotKnowAUsername <38732387+DidNotKnowAUsername@users.noreply.github.com>
2020-06-04 11:23:32 +0200

    Add files via upload

--- test.sh
+++ test.sh
@@ -1,5 +1,8 @@
-#Op de headnode
+#Variabele aanmaken
+string = hostname
 
+#Op de headnode
+if [[ $string == *"vm1"* ]]; then
 #update yum
 yum update -y
 
@@ -26,40 +29,45 @@ dd if=/dev/urandom bs=1 count=1024 > /etc/munge/munge.key
 chown munge: /etc/munge/munge.key
 chmod 400 /etc/munge/munge.key
 
+fi
+
+if  [ $string != *"vm1"* ] && [ $string == *"vm"* ]; then
 #dit moet gedaan worden op een andere node dit kan natuurlijk pas zodra alle nodes uitgeroled zijn:
 
-#Munge key kopieeeren naar de verschillende nodes:
-#scp -p /etc/munge/munge.key nodexxx:/etc/munge/munge.key   #<-- vannuit de headnode
+#Munge key kopieeëren naar de verschillende nodes:
+scp -p /etc/munge/munge.key $string:/etc/munge/munge.key   #<-- vannuit de headnode
 
 #services starten
-#systemctl enable munge
-#systemctl start  munge
+systemctl enable munge
+systemctl start  munge
 
 #op de nodes:
 
 #update yum
-#yum update -y
+yum update -y
 
 #epel repository:
-#yum install epel-release -y
+yum install epel-release -y
 
 #install munge
-#yum install munge munge-libs munge-devel -y
+yum install munge munge-libs munge-devel -y
 
 #Munge opzetten
-#export MUNGEUSER=1001
-#groupadd -g $MUNGEUSER munge
-#useradd  -m -c "MUNGE Uid 'N' Gid Emporium" -d /var/lib/munge -u $MUNGEUSER -g munge  -s /sbin/nologin munge
-#export SlurmUSER=1002
-#groupadd -g $SlurmUSER slurm
-#useradd  -m -c "Slurm workload manager" -d /var/lib/slurm -u $SlurmUSER -g slurm  -s /bin/bash slurm
+export MUNGEUSER=1001
+groupadd -g $MUNGEUSER munge
+useradd  -m -c "MUNGE Uid 'N' Gid Emporium" -d /var/lib/munge -u $MUNGEUSER -g munge  -s /sbin/nologin munge
+export SlurmUSER=1002
+groupadd -g $SlurmUSER slurm
+useradd  -m -c "Slurm workload manager" -d /var/lib/slurm -u $SlurmUSER -g slurm  -s /bin/bash slurm
 
 #zodra de munge key overgekopieerd is:
-#chown -R munge: /etc/munge/ /var/log/munge/
-#chmod 0700 /etc/munge/ /var/log/munge/
+chown -R munge: /etc/munge/ /var/log/munge/
+chmod 0700 /etc/munge/ /var/log/munge/
 
 #services starten
-#systemctl enable munge
-#systemctl start  munge
+systemctl enable munge
+systemctl start  munge
 
 #https://wiki.fysik.dtu.dk/niflheim/Slurm_installation
+
+fi
